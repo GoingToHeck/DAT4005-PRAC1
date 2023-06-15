0f76bcd1034586df4a9ed21081ccc8749ae088cc
victorvintorez <victorvintorez@proton.me>
2023-06-16 00:09:35 +0100

    Final Code

--- main.py
+++ main.py
@@ -1,134 +1,192 @@
 import re
 import argparse
 
-# Setup Arguments
+# -- Setup Arguments --
+# --infile / -if: REQUIRED, the path to the input file, example: -if ./input.txt
+# --outfile / -of: REQUIRED, the path to the output file, example: -of ./output.txt
+# --showparsed / -sp: OPTIONAL, show the parsed code in the console, example: -sp
 parser = argparse.ArgumentParser(prog="Compiler")
-parser.add_argument("filepath", help="The path to the file to be compiled")
+parser.add_argument("-if", "--infile", help="The path to the input file")
+parser.add_argument("-of", "--outfile", help="The path to the output file")
+parser.add_argument("-sp", "--showparsed", help="Show the parsed code in the console",
+                    action=argparse.BooleanOptionalAction)
 parsed_args = parser.parse_args()
+if not parsed_args.infile and not parsed_args.outfile:
+    raise Exception(
+        "Error: No arguments specified, please specify an input file and output file with the -if and -of flags: -if <path> -of <path>")
+if not parsed_args.infile:
+    raise Exception("Error: No input file specified, please specify an input file with the -if flag: -if <path>")
+if not parsed_args.outfile:
+    raise Exception("Error: No output file specified, please specify an output file with the -of flag: -of <path>")
 
-# Token Types
-TOKEN_IF = 'IF'
-TOKEN_ELSE = 'ELSE'
-TOKEN_OPEN_BRACE = 'OPEN_BRACE'
-TOKEN_CLOSE_BRACE = 'CLOSE_BRACE'
-TOKEN_GREATER_THAN = 'GREATER_THAN'
-TOKEN_LESS_THAN = 'LESS_THAN'
-TOKEN_GREATER_THAN_EQUAL = 'GREATER_THAN_EQUAL'
-TOKEN_LESS_THAN_EQUAL = 'LESS_THAN_EQUAL'
-TOKEN_EQUAL = 'EQUAL'
-TOKEN_VARIABLE_IDENTIFIER = 'VARIABLE_IDENTIFIER'
-TOKEN_VARIABLE_ASSIGNMENT = 'VARIABLE_ASSIGNMENT'
-TOKEN_WHILE = 'WHILE'
-TOKEN_PRINT = 'PRINT'
-TOKEN_LINE_COMMENT = 'LINE_COMMENT'
-TOKEN_OPEN_BLOCK_COMMENT = 'OPEN_BLOCK_COMMENT'
-TOKEN_CLOSE_BLOCK_COMMENT = 'CLOSE_BLOCK_COMMENT'
-TOKEN_TYPE_INT = 'TYPE_INT'
-TOKEN_TYPE_STRING = 'TYPE_STRING'
-TOKEN_TYPE_BOOL = 'TYPE_BOOLEAN'
-TOKEN_STRING = 'STRING'
-TOKEN_INT = 'INT'
-TOKEN_BOOL_TRUE = 'BOOL_TRUE'
-TOKEN_BOOL_FALSE = 'BOOL_FALSE'
-TOKEN_ARITH_PLUS = 'PLUS'
-TOKEN_ARITH_MINUS = 'MINUS'
-TOKEN_ARITH_MULTIPLY = 'MULTIPLY'
-TOKEN_ARITH_DIVIDE = 'DIVIDE'
 
-# Token Patterns
-TOKEN_PATTERNS = [
-    (r'//', TOKEN_LINE_COMMENT),
-    (r'/\*', TOKEN_OPEN_BLOCK_COMMENT),
-    (r'\*/', TOKEN_CLOSE_BLOCK_COMMENT),
-    (r'provided', TOKEN_IF),
-    (r'contrarily', TOKEN_ELSE),
-    (r'commence', TOKEN_OPEN_BRACE),
-    (r'conclude', TOKEN_CLOSE_BRACE),
-    (r'corresponds', TOKEN_VARIABLE_ASSIGNMENT),
-    (r'supposing', TOKEN_WHILE),
-    (r'resultant', TOKEN_PRINT),
-    (r'>', TOKEN_GREATER_THAN),
-    (r'<', TOKEN_LESS_THAN),
-    (r'>=', TOKEN_GREATER_THAN_EQUAL),
-    (r'<=', TOKEN_LESS_THAN_EQUAL),
-    (r'==', TOKEN_EQUAL),
-    (r'\+', TOKEN_ARITH_PLUS),
-    (r'-', TOKEN_ARITH_MINUS),
-    (r'\*', TOKEN_ARITH_MULTIPLY),
-    (r'/', TOKEN_ARITH_DIVIDE),
-    (r'int', TOKEN_TYPE_INT),
-    (r'string', TOKEN_TYPE_STRING),
-    (r'bool', TOKEN_TYPE_BOOL),
-    (r'true', TOKEN_BOOL_TRUE),
-    (r'false', TOKEN_BOOL_FALSE),
-    (r'".+?"', TOKEN_STRING),
-    (r'\d+', TOKEN_INT),
-    (r'\w+', TOKEN_VARIABLE_IDENTIFIER),
+# Token Types (Using a class because python match-case statements
+# don't support comparison against variables [for some reason], using a class somehow works)
+# https://stackoverflow.com/questions/67525257/capture-makes-remaining-patterns-unreachable
+class Tokens:
+    IF = 'IF'
+    ELSE = 'ELSE'
+    OPEN_BRACE = 'OPEN_BRACE'
+    CLOSE_BRACE = 'CLOSE_BRACE'
+    GREATER_THAN = 'GREATER_THAN'
+    LESS_THAN = 'LESS_THAN'
+    GREATER_THAN_EQUAL = 'GREATER_THAN_EQUAL'
+    LESS_THAN_EQUAL = 'LESS_THAN_EQUAL'
+    EQUAL = 'EQUAL'
+    VARIABLE_IDENTIFIER = 'VARIABLE_IDENTIFIER'
+    VARIABLE_ASSIGNMENT = 'VARIABLE_ASSIGNMENT'
+    WHILE = 'WHILE'
+    PRINT = 'PRINT'
+    LINE_COMMENT = 'LINE_COMMENT'
+    OPEN_BLOCK_COMMENT = 'OPEN_BLOCK_COMMENT'
+    CLOSE_BLOCK_COMMENT = 'CLOSE_BLOCK_COMMENT'
+    TYPE_INT = 'TYPE_INT'
+    TYPE_STRING = 'TYPE_STRING'
+    TYPE_BOOL = 'TYPE_BOOLEAN'
+    STRING = 'STRING'
+    INT = 'INT'
+    BOOL_TRUE = 'BOOL_TRUE'
+    BOOL_FALSE = 'BOOL_FALSE'
+    PLUS = 'PLUS'
+    MINUS = 'MINUS'
+    MULTIPLY = 'MULTIPLY'
+    DIVIDE = 'DIVIDE'
+
+
+# Token Regex Patterns, Variable Identifier is last because it is the most general
+TOKEN_PATTERN_MAP = [
+    (r'//', Tokens.LINE_COMMENT),
+    (r'/\*', Tokens.OPEN_BLOCK_COMMENT),
+    (r'\*/', Tokens.CLOSE_BLOCK_COMMENT),
+    (r'provided', Tokens.IF),
+    (r'contrarily', Tokens.ELSE),
+    (r'commence', Tokens.OPEN_BRACE),
+    (r'conclude', Tokens.CLOSE_BRACE),
+    (r'corresponds', Tokens.VARIABLE_ASSIGNMENT),
+    (r'supposing', Tokens.WHILE),
+    (r'resultant', Tokens.PRINT),
+    (r'>', Tokens.GREATER_THAN),
+    (r'<', Tokens.LESS_THAN),
+    (r'>=', Tokens.GREATER_THAN_EQUAL),
+    (r'<=', Tokens.LESS_THAN_EQUAL),
+    (r'==', Tokens.EQUAL),
+    (r'\+', Tokens.PLUS),
+    (r'-', Tokens.MINUS),
+    (r'\*', Tokens.MULTIPLY),
+    (r'/', Tokens.DIVIDE),
+    (r'int', Tokens.TYPE_INT),
+    (r'string', Tokens.TYPE_STRING),
+    (r'bool', Tokens.TYPE_BOOL),
+    (r'true', Tokens.BOOL_TRUE),
+    (r'false', Tokens.BOOL_FALSE),
+    (r'".+?"', Tokens.STRING),
+    (r'\d+', Tokens.INT),
+    (r'\w+', Tokens.VARIABLE_IDENTIFIER),
 ]
 
+# Replacements to be used in the pseudocode generator, uses string templates to use with the .format() function
+# to achieve desired output format for values.
+TOKEN_REPLACEMENT_MAP = dict([
+    (Tokens.IF, 'IF'),
+    (Tokens.ELSE, 'ELSE'),
+    (Tokens.OPEN_BRACE, '{'),
+    (Tokens.CLOSE_BRACE, '}'),
+    (Tokens.GREATER_THAN, '>'),
+    (Tokens.LESS_THAN, '<'),
+    (Tokens.GREATER_THAN_EQUAL, '>='),
+    (Tokens.LESS_THAN_EQUAL, '<='),
+    (Tokens.EQUAL, '=='),
+    (Tokens.VARIABLE_IDENTIFIER, '{0}'),
+    (Tokens.VARIABLE_ASSIGNMENT, ':='),
+    (Tokens.WHILE, 'WHILE'),
+    (Tokens.PRINT, 'PRINT'),
+    (Tokens.LINE_COMMENT, '#'),
+    (Tokens.OPEN_BLOCK_COMMENT, '/*'),
+    (Tokens.CLOSE_BLOCK_COMMENT, '*/'),
+    (Tokens.TYPE_INT, 'int var'),
+    (Tokens.TYPE_STRING, 'string var'),
+    (Tokens.TYPE_BOOL, 'bool var'),
+    (Tokens.STRING, '{0}'),
+    (Tokens.INT, '{0}'),
+    (Tokens.BOOL_TRUE, 'true'),
+    (Tokens.BOOL_FALSE, 'false'),
+    (Tokens.PLUS, '+'),
+    (Tokens.MINUS, '-'),
+    (Tokens.MULTIPLY, '*'),
+    (Tokens.DIVIDE, '/'),
+])
 
-# Load file and return string
+
+# Load file (using path from -if arg) and return file as string with newlines and tabs stripped
 def load_in_code_file(args):
     array = []
-    with open(args.filepath) as file:
+    with open(args.infile) as file:
         for line in file.readlines():
             array.append(line.strip('\n\t'))
     return " ".join(array)
 
 
-# Load parser table and return dictionary
+# Load parser table and return terminals, non-terminals, and parse table
 def load_in_parser_table():
     terminals = []
     non_terminals = []
     parse_table = []
-    with open("parse_table.csv") as file:
+    with open("parse_table.csv") as file:  # CSV IS REQUIRED TO BE IN THE SAME DIRECTORY AS THE PARSER
         for line in file.readlines():
             items = line.strip('\n').split(',')
             if not terminals:
-                terminals = items[1:]
+                terminals = items[1:]  # Collect terminals from column headers (without first column as it is empty)
             else:
-                non_terminals.append(items[0])
+                non_terminals.append(items[0])  # Collect non-terminals from first column of each row
                 parse_table.append(items[1:])
     return terminals, non_terminals, parse_table
 
 
-# Take string input and return tokens
+# Load pseudocode output into file (using path from -of arg)
+def create_output_file(output, args):
+    with open(args.outfile, "w") as file:
+        file.write(output)
+
+
+# Take string input (from load_in_code_file()) and return tokens
 def tokenizer(string_input):
     tokens = []
     current_index = 0
 
-    while current_index < len(string_input):
+    while current_index < len(string_input):  # Loop until end of string
         match = None
 
-        for token_pattern, token_type in TOKEN_PATTERNS:
+        for token_pattern, token_type in TOKEN_PATTERN_MAP:  # Check each token pattern
             pattern = re.compile(token_pattern)
             match = pattern.match(string_input, current_index)
 
             if match:
-                tokens.append((token_type, match.group()))
+                tokens.append((token_type, match.group()))  # Add token and matched value to tokens list
                 break
 
         if match:
-            current_index += match.end() - match.start()
+            current_index += match.end() - match.start()  # Increment index by length of matched value
         else:
             current_index += 1
 
     return tokens
 
 
-# Take Tokens and parse
+# Take Tokens (from tokenizer()) and parse
 def parser(tokens):
     parsed_tokens = []
 
-    terminals, non_terminals, parse_table = load_in_parser_table()
+    terminals, non_terminals, parse_table = load_in_parser_table()  # Get values from loaded parse table
 
     token_index = 0
     stack = ['<statement>']
 
     while True:
+        # Load a new statement if the current statement has been parsed and there are more tokens to parse
         if token_index + 1 < len(tokens):
             if tokens[token_index + 1] and not stack:
                 stack.extend(['<statement>'])
+
         top = stack[-1]
         current_token = tokens[token_index][0]
 
@@ -141,11 +199,13 @@ def parser(tokens):
                 raise Exception("Error: Invalid Token: " + current_token)
             stack.pop()
 
-            # duct tape fix because I couldn't get the else clause to work properly
+            # duct tape fix because I couldn't get the else clause to work properly (not sure what I did wrong,
+            # please let me know in my feedback)
             if top == '<else-clause>':
-                if tokens[token_index + 1][0] == TOKEN_OPEN_BRACE:
+                if tokens[token_index + 1][0] == Tokens.OPEN_BRACE:
                     production_rule = 18
 
+            # Use a switch statement to determine which production rule to use (helps with readability)
             match int(production_rule):
                 case 1:
                     stack.extend(['<variable-statement>'])
@@ -168,7 +228,7 @@ def parser(tokens):
                 case 10:
                     stack.extend(['<assignment>', '<variable-name>'])
                 case 11:
-                    stack.extend(['<arithmetic-statement>', '<variable-name>'])
+                    stack.extend(['<arithmetic-statement>', 'VARIABLE_ASSIGNMENT', '<variable-name>'])
                 case 12:
                     stack.extend(['<value>', 'VARIABLE_ASSIGNMENT'])
                 case 13:
@@ -264,42 +324,71 @@ def parser(tokens):
                     raise Exception("Production Rule Not Found: " + production_rule)
 
         elif top == current_token:
-            parsed_tokens.append(current_token)
+            parsed_tokens.append(current_token)  # if token is terminal matching top of stack, add to parsed tokens
             stack.pop()
             token_index += 1
 
-            if token_index >= len(tokens) and not stack:
+            if token_index >= len(tokens) and not stack:  # if stack is empty and no more tokens, parsing is complete
                 print("Successfully Parsed Tokens")
                 return parsed_tokens
 
 
-# Take Tokens and pass through Semantic Analyzer
+# Take Tokens (from tokenizer()) and pass through Semantic Analyzer
 def semantic_analyzer(parsed_tokens):
-    for i in range(len(parsed_tokens)):
-        match parsed_tokens[i]:
+    for i in range(len(parsed_tokens)):  # iterate through tokens
+        match parsed_tokens[i]:  # use switch statement to determine which token is being analyzed
             case 'TYPE_STRING':
-                print(parsed_tokens[i + 3])
                 if parsed_tokens[i + 3] != 'STRING':
                     raise Exception("Semantic Error: STRING Type Not Assigned to STRING Value")
             case 'TYPE_INT':
-                print(parsed_tokens[i + 3])
                 if parsed_tokens[i + 3] != 'INT':
                     raise Exception("Semantic Error: INT Type Not Assigned to INT Value")
             case 'TYPE_BOOLEAN':
-                print(parsed_tokens[i + 3])
                 if parsed_tokens[i + 3] != 'BOOL_TRUE' and parsed_tokens[i + 3] != 'BOOL_FALSE':
                     raise Exception("Semantic Error: BOOLEAN Type Not Assigned to BOOLEAN Value")
             case _:
                 pass
 
 
-# Main Function
+# Generate Output from Tokens (from tokenizer())
+def generate_output(tokens):
+    output = []
+
+    comment_skip = 0
+    for i in range(len(tokens)):
+        token, value = tokens[i]
+
+        # If token is a comment, skip it and the related tokens
+        if token == 'LINE_COMMENT':
+            comment_skip = 1
+            continue
+        elif token == 'OPEN_BLOCK_COMMENT':
+            comment_skip = 2
+            continue
+
+        if comment_skip > 0:
+            comment_skip -= 1
+            continue
+
+        # Add token to output, including value if applicable (Try Except as the OPEN_BRACE and CLOSE_BRACE tokens cause
+        # ValueErrors when trying to format them due to the curly braces being used for formatting)
+        try:
+            output.append(TOKEN_REPLACEMENT_MAP[token].format(str(value)))
+        except ValueError:
+            output.append(TOKEN_REPLACEMENT_MAP[token])
+
+    return " ".join(output)  # return output as a string
+
+
+# Main Function (Each step has its own error handling so the program will stop at the first error)
 def __main__():
     file_input_string = load_in_code_file(parsed_args)
     tokens = tokenizer(file_input_string)
     parsed_tokens = parser(tokens)
-    print(parsed_tokens)
+    print("\n".join(parsed_tokens)) if parsed_args.showparsed is True else None  # print parsed tokens if flag is set
     semantic_analyzer(parsed_tokens)
+    output = generate_output(tokens)
+    create_output_file(output, parsed_args)
 
 
-__main__()
+__main__()  # Run Main Function
